6b283fd43946b665f273cdc0f757bfbddda768ed
Patty Rose <oteropatty@gmail.com>
2023-08-21 14:12:28 -0700

    add addItem ui form
    Co-Authored-by: Isaiah Sanchez <isaiahsanchez.ca@gmail.com>

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { addItem } from '../api/firebase';
+
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	const [itemName, setItemName] = useState('');
+	const [anticipation, setAnticipation] = useState('7');
+	const [message, setMessage] = useState('');
+
+	useEffect(() => {
+		console.log(anticipation);
+	}, [anticipation]);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		// Here you can implement the logic to save the item to the database
+		// You can use 'itemName' and 'anticipation' in this logic
+
+		setMessage(`Item '${itemName}' was saved to the database.`);
+	};
+
+	return (
+		<div>
+			<p>
+				Hello from the <code>/add-item</code> page!
+			</p>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="item-name">Item Name:</label>
+				<input
+					type="text"
+					id="item-name"
+					value={itemName}
+					onChange={(event) => setItemName(event.target.value)}
+					required
+				/>
+
+				<label htmlFor="anticipation">Anticipation:</label>
+				<select
+					id="anticipation"
+					value={anticipation}
+					onChange={(event) => setAnticipation(event.target.value)}
+				>
+					<option value="7">Soon (7 days)</option>
+					<option value="14">Kind of Soon (14 days)</option>
+					<option value="30">Not Soon (30 days)</option>
+				</select>
+
+				<button type="submit">Submit</button>
+			</form>
+
+			{message && <p>{message}</p>}
+		</div>
+	);
 }
