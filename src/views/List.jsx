b6f4832305f4522d980260dddd0b5f9738dc61ae
Patty Rose <oteropatty@gmail.com>
2023-08-28 18:03:30 -0700

    add filter form without filter functionality
    Co-authored-by: Shannone Ball <shannone426@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data, listToken }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
 	return (
 		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
+			<form>
+				<label htmlFor="search-term">Filter items:</label>
+				<input
+					type="text"
+					id="search-term"
+					value={searchTerm}
+					placeholder="Start typing here.."
+					onChange={(event) => setSearchTerm(event.target.value)}
+				/>
+			</form>
+
 			<ul>
 				{data.map((item) => (
 					<ListItem key={item.id} name={item.name} />
