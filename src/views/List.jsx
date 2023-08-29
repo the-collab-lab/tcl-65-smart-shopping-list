f176d4bdc8c8c27ca08812fb52f7ca6f5bd5b41a
Patty Rose <oteropatty@gmail.com>
2023-08-28 19:24:23 -0700

    add clear button to filter input
    Co-authored-by Shannone Ball <shannone426@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -5,25 +5,6 @@ export function List({ data, listToken }) {
 	const [searchTerm, setSearchTerm] = useState('');
 	const [filteredData, setFilteredData] = useState([]);
 
-	// useEffect(() => {
-	// 	const tempData = data.filter((item)=>item.name === searchTerm)
-	//   setFilteredData(tempData);
-	// }, [searchTerm]);
-
-	// useEffect(() => {
-	// 	setFilteredData(data);
-	// }, [data]);
-
-	// const handleSearchInput = (e) => {
-	// 	const text = e.target.value;
-	// 	setSearchTerm(text);
-	// 	setFilteredData(
-	// 		data.filter((item) =>
-	// 			item.name.toLowerCase().includes(text.toLowerCase()),
-	// 		),
-	// 	);
-	// };
-
 	// Function to update the filteredData based on the searchTerm
 	const updateFilteredData = () => {
 		const filteredItems = data.filter((item) => {
@@ -39,6 +20,11 @@ export function List({ data, listToken }) {
 		updateFilteredData();
 	}, [searchTerm, data]);
 
+	const handleClear = () => {
+		setSearchTerm('');
+		setFilteredData(data);
+	};
+
 	return (
 		<>
 			<form className="filter-form">
@@ -50,19 +36,17 @@ export function List({ data, listToken }) {
 					placeholder="Start typing here.."
 					onChange={(event) => setSearchTerm(event.target.value)}
 				></input>
+				{searchTerm.length > 0 && (
+					<button type="reset" className="clear" onClick={handleClear}>
+						X
+					</button>
+				)}
 			</form>
 
 			<ul>
 				{filteredData.map((item) => (
 					<ListItem key={item.id} name={item.name} />
 				))}
-				{/* {data
-					.filter((item) =>
-						item.name.toLowerCase().includes(searchTerm.toLowerCase()),
-					)
-					.map((item) => (
-						<ListItem key={item.id} name={item.name} />
-					))} */}
 			</ul>
 		</>
 	);
