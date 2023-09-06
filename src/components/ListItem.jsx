d57cb00bc8d39d2d18f57ca2b1ced66c83ac0bfa
shannone <shannone426@gmail.com>
2023-09-05 20:34:32 -0400

    commented out some other attemps at getting this to run with no success" "

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,19 @@
 import './ListItem.css';
+//import { isWithinLastDay } from '../utils/dates';
 //import {updateItem} from '../api/firebase'
+
+/*export function ListItem({ item, listId, handleDeleteConfirmation, urgency }) {
+	const { itemId, name, dateLastPurchased, dateNextPurchased, totalPurchases } =
+		item;
+
+	const lastPurchasedDate = dateLastPurchased?.toDate().toDateString();
+	const nextPurchasedDate = dateNextPurchased?.toDate().toDateString();
+
+	const handlePurchase = async (e) => {
+		if (e.target.checked) {
+			await updateItem(listId, item);
+		}
+	};*/
 
 export function ListItem({
 	name,
@@ -24,6 +38,8 @@ export function ListItem({
 					//checked={oneDayInMillis(item.dateLastPurchased)}
 					checked={itemPurchased()}
 					onChange={() => updateItem(itemId, dateLastPurchased, totalPurchases)}
+					//onChange={handlePurchase}
+					//isChecked={isWithinLastDay(item.dateLastPurchased)}
 				/>
 				{name}
 			</label>
