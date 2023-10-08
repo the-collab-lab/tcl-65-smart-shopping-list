6380ddca6ba5905c44472bd5710ea041ca0914e5
Lydia Dunning <lydia.dunning@gmail.com>
2023-10-07 18:12:15 -0700

    Home page welcome screen

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -23,9 +23,6 @@ export function Home({ listToken, setListToken, handleNewToken }) {
 
 	const currentListDisplay = (
 		<div className="current-list-display">
-			<p>
-				Current list token: <strong>{listToken}</strong>
-			</p>
 			<button
 				className="button-medium"
 				onClick={() => setShowSwitchListForm(!showSwitchListForm)}
@@ -36,30 +33,34 @@ export function Home({ listToken, setListToken, handleNewToken }) {
 	);
 
 	const switchListForm = (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor="list-token">Enter Token</label>
-			<input
-				type="text"
-				id="list-token"
-				placeholder="Start typing here.."
-				value={inputToken}
-				onChange={(event) => setInputToken(event.target.value)}
-				required
-			/>
-			<button className="button-medium" type="submit">
-				Join List
-			</button>
-			{tokenNotFoundMessage && <p>{tokenNotFoundMessage}</p>}
-		</form>
+		<>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="list-token">Enter Token</label>
+				<input
+					type="text"
+					id="list-token"
+					placeholder="Start typing here.."
+					value={inputToken}
+					onChange={(event) => setInputToken(event.target.value)}
+					required
+				/>
+				<button className="button-medium" type="submit">
+					Join List
+				</button>
+			</form>
+			{tokenNotFoundMessage ? (
+				<p className="error-message">{tokenNotFoundMessage}</p>
+			) : (
+				<br />
+			)}
+		</>
 	);
 
-	return (
-		<div className="Home">
+	const welcome = (
+		<>
 			<p>Let's get started with a new shopping list!</p>
 			<p className="spacer"></p> {/* Adds more space below the text */}
 			<div className="centered-container">
-				{listToken && currentListDisplay}
-				{(!listToken || showSwitchListForm) && switchListForm}
 				<Link to="/list">
 					<button className="button-medium" onClick={handleNewToken}>
 						Create a New List
@@ -69,7 +70,6 @@ export function Home({ listToken, setListToken, handleNewToken }) {
 				{/* Adds space below the "Create a New List" button */}
 				<p className="or-text">-or-</p> {/* Adds a class for styling "-or-" */}
 				<p className="spacer"></p> {/* Adds space below "-or-" */}
-				{tokenNotFoundMessage && <p>{tokenNotFoundMessage}</p>}
 				<form onSubmit={handleSubmit}>
 					<label htmlFor="list-token">
 						Enter a token below to join an existing list
@@ -87,8 +87,28 @@ export function Home({ listToken, setListToken, handleNewToken }) {
 					<button className="button-medium" type="submit">
 						Join List
 					</button>
+					{tokenNotFoundMessage && (
+						<p className="error-message">{tokenNotFoundMessage}</p>
+					)}
 				</form>
 			</div>
-		</div>
+		</>
 	);
+
+	const welcomeBack = (
+		<>
+			<p className="spacer"></p> {/* Adds more space below the text */}
+			<div className="centered-container">
+				{listToken && currentListDisplay}
+				{(!listToken || showSwitchListForm) && switchListForm}
+				<Link to="/list">
+					<button className="button-medium" onClick={handleNewToken}>
+						Create a New List
+					</button>
+				</Link>
+			</div>
+		</>
+	);
+
+	return <div className="Home">{listToken ? welcomeBack : welcome}</div>;
 }
